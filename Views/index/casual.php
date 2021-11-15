947048aee8a8dfc752243db5fbc7d04b5189ea9b
JuanfelipeX <pipesjimenez6@gmail.com>
2021-11-15 12:34:57 -0500

    agregando otro carro a casual arreglar buton

--- Views/index/casual.php
+++ Views/index/casual.php
@@ -21,10 +21,10 @@ include 'menu.php';
     <div class="texto-huracan1">
         <img src="<?php echo IMG_PATH; ?>car5.gif" alt="Carro 5" width="1250" height="600">
         <div class="texto-huracan2">
-            <h2 style="color: white;">MAZDA 3 PRIME MT</h2>
+            <h1 style="color: white;">MAZDA 3 PRIME MT</h1>
         </div>
         <div class="texto-huracan3">
-            <h1 style="color: white;"> ¡INSPIRADO EN LA <br> GRANDEZA HUMANA! </h1>
+            <h3 style="color: white;"> ¡INSPIRADO EN LA <br> GRANDEZA HUMANA! </h3>
         </div>
     </div>
 
@@ -54,6 +54,56 @@ include 'menu.php';
     <br>
 
     <iframe width="1200" height="500" src="https://www.youtube.com/embed/sP46ZpVMO54" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <br><br>
+
+    <div align="center"><button type="button" class="btn btn-primary">Comprar</button></div>
+
+    <br><br>
+
+
+
+    <div class="texto-huracan1">
+        <img src="<?php echo IMG_PATH; ?>car8.gif" alt="Carro 8" width="1250" height="600">
+        <div class="texto-huracan2">
+            <h1 style="color: white;">NISSAN KICKS</h1>
+        </div>
+        <div class="texto-huracan3">
+            <h3 style="color: white;"> AMIGABLE A LA <br> VISTA </h3>
+        </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="card bg-secondary mb-3" style="max-width: 70rem;">
+        <div class="card-header">CARACTERISTICAS</div>
+        <div class="card-body">
+            <h4 class="card-title">POTENCIA &nbsp;&nbsp;&nbsp; NM &nbsp;&nbsp;&nbsp; NUMERO DE PASAJEROS </h4>
+            <p class="card-text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 118HP &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 149 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 5</p>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <img src="<?php echo IMG_PATH; ?>car11.png" alt="Carro 11" width="600" height="400">
+            </div>
+            <div class="col-sm">
+                <h1>AMIGABLE PARA <br> TODOS </h1 <h6> El diseño interior del nuevo Nissan KICKS es amigable con todos. La amplitud de su cabina, su sistema ISOFIX, la consola central y sus 8 bocinas Bose® están pensadas para un viaje más confortable. <br> <br> El Nuevo Nissan Kicks ha sido rediseñado para que tengas todo el confort que necesitas y así poder disfrutar tu camino desde cualquier lugar dentro de tu Crossover. *Consulta equipamiento y aditamentos especiales por versión.</h6> <br> <br>
+            </div>
+        </div>
+    </div>
+
+    <br>
+
+    <iframe width="1200" height="500" src="https://www.youtube.com/embed/T5CVdz7jMGQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <br><br>
+
+    <div align="center"><button type="button" class="btn btn-primary">Comprar</button></div>
+
+    <br><br>
 
 </div> <!-- cierra container-fluid-->
 </div> <!-- cierra float-left--->
